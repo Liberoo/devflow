9f1a5dac3c587a2398d2465f11f88ba863454447
Kamil <kamil-jamroz@o2.pl>
2023-09-28 13:02:16 +0200

    :FIX: Formating .css files

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,7 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
     <div>
-      <h1>Next.js Next.js 13 we're coming!</h1>
+      <h1>Next.js Next.js 13 we &apos e coming!</h1>
     </div>
   );
 }
